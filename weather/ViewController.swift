828b405c75e4242dbb59fa71a15ebb0d3d31e014
肖文禹 <xiaowenyu@xiaowenyudemacbook-pro.local>
2016-09-14 19:56:06 +0800

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by 肖文禹 on 16/9/14.
+//  Copyright © 2016年 肖文禹. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
