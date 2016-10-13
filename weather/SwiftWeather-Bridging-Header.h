c967e1fb6ba0cc96d3e3cfde1e6d49639698c765
xwy988143 <312503324@qq.com>
2016-10-13 15:53:06 +0800

    weather

---
+++ weather/SwiftWeather-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftWeather-Bridging-Header.h
+//  weather
+//
+//  Created by 肖文禹 on 16/9/15.
+//  Copyright © 2016年 肖文禹. All rights reserved.
+//
+#import <AFNetworking/AFNetworking.h>
+#ifndef SwiftWeather_Bridging_Header_h
+#define SwiftWeather_Bridging_Header_h
+
+
+#endif /* SwiftWeather_Bridging_Header_h */
--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    let locationManger:CLLocationManager =
+        CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManger.delegate = self
+        locationManger.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if (ios8()) {
+            locationManger.requestAlwaysAuthorization()
+        }
+        locationManger.startUpdatingLocation()
+    }
+    
+    func ios8() -> Bool
+    {
+        let version = NSString(string: UIDevice.current.systemVersion).floatValue
+        
+        return version >= 8.0
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location:CLLocation = locations[locations.count - 1] as! CLLocation
+        
+        if location.horizontalAccuracy > 0 {
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            self.updateWeatherInfo(latitude: location.coordinate.latitude,longtitude:location.coordinate.longitude)
+            locationManger.stopUpdatingLocation()
+        }
+    }
+    func updateWeatherInfo(latitude:CLLocationDegrees,longtitude:CLLocationDegrees)
+    {
+        let manager = AFHTTPRequestSerializer()
+        let url = "http://api.openweathermap.org/data/2.5/weather"
+        let params = ["lat":latitude,"lon":longtitude,"cnt":0]
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print(error)
+    }
 
 }
 
